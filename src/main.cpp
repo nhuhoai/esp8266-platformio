22c0a0176f7299e2e2ee50ef1e9764ba50d7000f
Nhu-Hoai Robert VO <nhuhoai.vo@franicflow.ch>
2018-02-11 15:09:06 +0100

    Create blink project.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
+/**
+ *  @file     main.cpp
+ *  @version  0.1.0
+ *  @date     2018-02-11
+ *  @author   Nhu-Hoai Robert VO <nhuhoai.vo@nhuvo.ch>
+ *  @brief    Blinking built-in led
+ */
+
 #include <Arduino.h>
 
 void setup() {
-    // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
 }
